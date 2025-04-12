8ed716f9a8e8978a7562cc1e57ecdbd5e758fd9e
Goldyban Nastya <nasty-goldyba@ya.ru>
2025-04-12 16:16:17 +0300

    change ci.yml file

--- config/settings.py
+++ config/settings.py
@@ -83,7 +83,7 @@ if "test" in sys.argv:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "test_db.sqlite3"
+            "NAME": os.path.join(BASE_DIR, 'test_db.sqlite3')
         }
     }
 
