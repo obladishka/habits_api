f939666dab03e1de65979372104624675946b634
Goldyban Nastya <nasty-goldyba@ya.ru>
2025-04-12 18:08:26 +0300

    add gunicorn

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,10 @@
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
+
+from config import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -9,3 +13,7 @@ urlpatterns = [
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularRedocView.as_view(url_name="schema"), name="docs"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
